f4f71072b9820329d0985c9e95eaa754838305ee
Joshua Lampert <joshua.lampert@tuhh.de>
2023-05-07 22:38:41 +0200

    fix l2errors in tests

--- test/test_bbm_bbm_1d.jl
+++ test/test_bbm_bbm_1d.jl
@@ -15,18 +15,18 @@ isdir(outdir) && rm(outdir, recursive=true)
     include(joinpath(EXAMPLES_DIR, "bbm_bbm_1d_basic.jl"))
     analytical_sol = @test_nowarn DispersiveShallowWater.compute_coefficients(initial_condition, solver, tspan[end], equations, mesh)
     l2error_eta =  integrate(u->u^2, sol.u[end][1, :] - analytical_sol[1, :], solver.D) |> sqrt
-    @test isapprox(l2error_eta, 0.515572239890494)
+    @test isapprox(l2error_eta, 0.5155722604645978, atol=500*eps(), sqrt(eps()))
     l2error_v =  integrate(u->u^2, sol.u[end][2, :] - analytical_sol[2, :], solver.D) |> sqrt
-    @test isapprox(l2error_v, 0.489001894785943)
+    @test isapprox(l2error_v, 0.48900191864693715, atol=500*eps(), sqrt(eps()))
   end
 
   @testset "bbm_bbm_1d_dg" begin
     include(joinpath(EXAMPLES_DIR, "bbm_bbm_1d_dg.jl"))
     analytical_sol = @test_nowarn DispersiveShallowWater.compute_coefficients(initial_condition, solver, tspan[end], equations, mesh)
     l2error_eta =  integrate(u->u^2, sol.u[end][1, :] - analytical_sol[1, :], solver.D) |> sqrt
-    @test isapprox(l2error_eta, 1.5909692913718887)
+    @test isapprox(l2error_eta, 2.029527814597151, atol=500*eps(), sqrt(eps()))
     l2error_v =  integrate(u->u^2, sol.u[end][2, :] - analytical_sol[2, :], solver.D) |> sqrt
-    @test isapprox(l2error_v, 1.5139913663032467)
+    @test isapprox(l2error_v, 1.9434053327388578, atol=500*eps(), sqrt(eps()))
   end
 end
 
