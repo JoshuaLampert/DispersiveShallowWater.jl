9a35928f87238a1d974d5a43c302f9c394826a12
Joshua Lampert <joshua.lampert@tuhh.de>
2023-05-28 19:02:13 +0200

    fix CI

--- src/semidiscretization.jl
+++ src/semidiscretization.jl
@@ -38,7 +38,7 @@ end
                        boundary_conditions=boundary_condition_periodic,
                        RealT=real(solver),
                        uEltype=RealT,
-                      initial_cache=NamedTuple())
+                       initial_cache=NamedTuple())
 
 Construct a semidiscretization of a PDE.
 """
