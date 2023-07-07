1d0de18c5e5805e85735d5316bab16d70a7b844b
Joshua Lampert <joshua.lampert@tuhh.de>
2023-07-07 19:32:41 +0200

    return errors and integrals as named tuples

--- test/test_bbm_bbm_variable_bathymetry_1d.jl
+++ test/test_bbm_bbm_variable_bathymetry_1d.jl
@@ -9,100 +9,64 @@ EXAMPLES_DIR = joinpath(examples_dir(), "bbm_bbm_variable_bathymetry_1d")
 
 @testset "BBMBBMVariableBathymetry1D" begin
     @trixi_testset "bbm_bbm_variable_bathymetry_1d_basic" begin
-        trixi_include(@__MODULE__,
-                      joinpath(EXAMPLES_DIR, "bbm_bbm_variable_bathymetry_1d_basic.jl"),
-                      tspan = (0.0, 1.0))
-        errs = @view errors(analysis_callback)[:, :, end]
-        @test isapprox(errs,
-                       [0.04346190082228665 0.11171710004736789 0.0
-                        0.04283944204214696 0.0953750884645661 0.0
-                        1.0394561372181596e-13 1.1368683772161603e-13 0.0],
-                       atol = 500 * eps(), rtol = sqrt(eps()))
-        change_of_invariants = integrals(analysis_callback)[:, end] -
-                               integrals(analysis_callback)[:, 1]
-        @test isapprox(change_of_invariants,
-                       [
-                           1.0394561372181596e-13,
-                           1.1368683772161603e-13,
-                           0.06735422396923241,
-                       ],
-                       atol = 1e-11, rtol = sqrt(eps()))
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "bbm_bbm_variable_bathymetry_1d_basic.jl"),
+                            tspan=(0.0, 1.0),
+                            l2=[0.04346190082228665 0.11171710004736789 0.0],
+                            linf=[0.04283944204214696 0.0953750884645661 0.0],
+                            cons_error=[1.0394561372181596e-13 3.410605131648481e-13 0.0],
+                            change_waterheight=1.0394561372181596e-13,
+                            change_velocity=-3.410605131648481e-13,
+                            change_entropy=0.06735422396923241)
     end
 
     @trixi_testset "bbm_bbm_variable_bathymetry_1d_relaxation" begin
-        trixi_include(@__MODULE__,
-                      joinpath(EXAMPLES_DIR,
-                               "bbm_bbm_variable_bathymetry_1d_relaxation.jl"),
-                      tspan = (0.0, 1.0))
-        errs = @view errors(analysis_callback)[:, :, end]
-        @test isapprox(errs,
-                       [1.6263014524141195 2.1343189572239996 0.0
-                        2.020840307859494 2.6331707343176247 0.0
-                        2.0694557179012918e-13 2.789919043027633e-13 0.0],
-                       atol = 500 * eps(), rtol = sqrt(eps()))
-        change_of_invariants = integrals(analysis_callback)[:, end] -
-                               integrals(analysis_callback)[:, 1]
-        @test isapprox(change_of_invariants,
-                       [-2.1271873151818e-13, 2.7874259106214573e-13, 0.0],
-                       atol = 1e-11, rtol = sqrt(eps()))
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "bbm_bbm_variable_bathymetry_1d_relaxation.jl"),
+                            tspan=(0.0, 1.0),
+                            l2=[1.6263014524141195 2.1343189572239996 0.0],
+                            linf=[2.020840307859494 2.6331707343176247 0.0],
+                            cons_error=[2.0694557179012918e-13 2.789919043027633e-13 0.0],
+                            change_waterheight=-2.1271873151818e-13,
+                            change_velocity=2.7874259106214573e-13,
+                            change_entropy=0.0)
     end
 
     @trixi_testset "bbm_bbm_variable_bathymetry_1d_dg_upwind_relaxation" begin
-        trixi_include(@__MODULE__,
-                      joinpath(EXAMPLES_DIR,
-                               "bbm_bbm_variable_bathymetry_1d_dg_upwind_relaxation.jl"),
-                      tspan = (0.0, 1.0))
-        errs = @view errors(analysis_callback)[:, :, end]
-        @test isapprox(errs,
-                       [1.6263014468792105 2.134318981217363 0.0
-                        2.020841121130176 2.6332103195755705 0.0
-                        3.1796787425264483e-13 1.0584519372081047e-12 0.0],
-                       atol = 500 * eps(), rtol = sqrt(eps()))
-        change_of_invariants = integrals(analysis_callback)[:, end] -
-                               integrals(analysis_callback)[:, 1]
-        @test isapprox(change_of_invariants,
-                       [3.1796787425264483e-13
-                        -1.0584519372081047e-12
-                        1.4210854715202004e-14],
-                       atol = 1e-11, rtol = sqrt(eps()))
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "bbm_bbm_variable_bathymetry_1d_dg_upwind_relaxation.jl"),
+                            tspan=(0.0, 1.0),
+                            l2=[1.6263014468792105 2.134318981217363 0.0],
+                            linf=[2.020841121130176 2.6332103195755705 0.0],
+                            cons_error=[3.1796787425264483e-13 1.0584519372081047e-12 0.0],
+                            change_waterheight=3.1796787425264483e-13,
+                            change_velocity=-1.0584519372081047e-12,
+                            change_entropy=1.4210854715202004e-14)
     end
 
     @trixi_testset "bbm_bbm_variable_bathymetry_1d_upwind_relaxation" begin
-        trixi_include(@__MODULE__,
-                      joinpath(EXAMPLES_DIR,
-                               "bbm_bbm_variable_bathymetry_1d_upwind_relaxation.jl"),
-                      tspan = (0.0, 1.0))
-        errs = @view errors(analysis_callback)[:, :, end]
-        @test isapprox(errs,
-                       [1.626301439652868 2.134319022870026 0.0
-                        2.0208207914664107 2.633227224286964 0.0
-                        1.5765166949677223e-13 1.8791999206735355e-13 0.0],
-                       atol = 500 * eps(), rtol = sqrt(eps()))
-        change_of_invariants = integrals(analysis_callback)[:, end] -
-                               integrals(analysis_callback)[:, 1]
-        @test isapprox(change_of_invariants,
-                       [-1.5765166949677223e-13
-                        1.8791999206735355e-13
-                        2.1316282072803006e-14],
-                       atol = 1e-11, rtol = sqrt(eps()))
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "bbm_bbm_variable_bathymetry_1d_upwind_relaxation.jl"),
+                            tspan=(0.0, 1.0),
+                            l2=[1.626301439652868 2.134319022870026 0.0],
+                            linf=[2.0208207914664107 2.633227224286964 0.0],
+                            cons_error=[1.5765166949677223e-13 1.8791999206735355e-13 0.0],
+                            change_waterheight=-1.5765166949677223e-13,
+                            change_velocity=1.8791999206735355e-13,
+                            change_entropy=2.1316282072803006e-14)
     end
 
     @trixi_testset "bbm_bbm_variable_bathymetry_1d_well_balanced" begin
-        trixi_include(@__MODULE__,
-                      joinpath(EXAMPLES_DIR,
-                               "bbm_bbm_variable_bathymetry_1d_well_balanced.jl"),
-                      tspan = (0.0, 10.0))
-        errs = @view errors(analysis_callback)[:, :, end]
-        @test isapprox(errs,
-                       [6.313048635750119e-15 3.793418293562131e-14 0.0
-                        1.9317880628477724e-14 1.1898289293196742e-13 0.0
-                        0.0 5.728935765975126e-15 0.0],
-                       atol = 1e-11, rtol = sqrt(eps()))
-        change_of_invariants = integrals(analysis_callback)[:, end] -
-                               integrals(analysis_callback)[:, 1]
-        @test isapprox(change_of_invariants,
-                       [0.0, -5.728935765975126e-15, 0.0, 6.725397799854067e-15],
-                       atol = 1e-11, rtol = sqrt(eps()))
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "bbm_bbm_variable_bathymetry_1d_well_balanced.jl"),
+                            tspan=(0.0, 1.0),
+                            l2=[6.313048635750119e-15 3.793418293562131e-14 0.0],
+                            linf=[1.9317880628477724e-14 1.1898289293196742e-13 0.0],
+                            cons_error=[0.0 5.728935765975126e-15 0.0],
+                            change_waterheight=0.0,
+                            change_velocity=-5.728935765975126e-15,
+                            change_entropy=0.0,
+                            lake_at_rest=6.725397799854067e-15)
     end
 end
 
--- test/test_util.jl
+++ test/test_util.jl
@@ -1,3 +1,129 @@
+using Test: @test
+
+# Use a macro to avoid world age issues when defining new initial conditions etc.
+# inside an elixir.
+"""
+    @test_trixi_include(elixir; l2=nothing, linf=nothing, cons_error=nothing
+                                change_waterheight=nothing,
+                                change_velocity=nothing,
+                                change_entropy=nothing,
+                                lake_at_rest=nothing,
+                                atol=10*eps(), rtol=0.001)
+
+Test by calling `trixi_include(elixir; parameters...)`.
+By default, only the absence of error output is checked.
+If `l2`, `linf` or `cons_error` are specified, in addition the resulting L2/Linf/conservation
+errors are compared approximately against these reference values, using `atol, rtol`
+as absolute/relative tolerance.
+If `change_waterheight`, `change_velocity`, `change_entropy` or `lake_at_rest` are specified,
+in addition the resulting changes of the waterheight/velocity/entropy/lake-at-rest-error are
+compared approximately against these reference values, using `atol, rtol` as absolute/relative tolerance.
+"""
+macro test_trixi_include(elixir, args...)
+    local l2 = get_kwarg(args, :l2, nothing)
+    local linf = get_kwarg(args, :linf, nothing)
+    local cons_error = get_kwarg(args, :cons_error, nothing)
+    local change_waterheight = get_kwarg(args, :change_waterheight, nothing)
+    local change_velocity = get_kwarg(args, :change_velocity, nothing)
+    local change_entropy = get_kwarg(args, :change_entropy, nothing)
+    local lake_at_rest = get_kwarg(args, :lake_at_rest, nothing)
+    local atol = get_kwarg(args, :atol, 500 * eps())
+    local rtol = get_kwarg(args, :rtol, sqrt(eps()))
+
+    local kwargs = Pair{Symbol, Any}[]
+    for arg in args
+        if (arg.head == :(=) &&
+            !(arg.args[1] in (:l2, :linf, :cons_error, :change_waterheight,
+                              :change_velocity, :change_entropy, :atol, :rtol)))
+            push!(kwargs, Pair(arg.args...))
+        end
+    end
+
+    quote
+        println("═"^100)
+        println($elixir)
+
+        # evaluate examples in the scope of the module they're called from
+        @test_nowarn trixi_include(@__MODULE__, $elixir; $kwargs...)
+
+        # if present, compare l2, linf and conservation errors against reference values
+        if !isnothing($l2) || !isnothing($linf) || !isnothing($cons_error)
+            errs = errors(analysis_callback)
+
+            if !isnothing($l2)
+                l2_measured = errs.l2_error[:, end]
+                @test length($l2) == length(l2_measured)
+                for (l2_expected, l2_actual) in zip($l2, l2_measured)
+                    @test isapprox(l2_expected, l2_actual, atol = $atol, rtol = $rtol)
+                end
+            end
+
+            if !isnothing($linf)
+                linf_measured = errs.linf_error[:, end]
+                @test length($linf) == length(linf_measured)
+                for (linf_expected, linf_actual) in zip($linf, linf_measured)
+                    @test isapprox(linf_expected, linf_actual, atol = $atol, rtol = $rtol)
+                end
+            end
+
+            if !isnothing($cons_error)
+                cons_error_measured = errs.conservation_error[:, end]
+                @test length($cons_error) == length(cons_error_measured)
+                for (conservation_error_expected, conservation_error_actual) in zip($cons_error,
+                                                                                    cons_error_measured)
+                    @test isapprox(conservation_error_expected, conservation_error_actual,
+                                   atol = $atol, rtol = $rtol)
+                end
+            end
+        end
+
+        if !isnothing($change_waterheight) || !isnothing($change_velocity) ||
+           !isnothing($change_entropy) || !isnothing($lake_at_rest)
+            ints = integrals(analysis_callback)
+
+            if !isnothing($change_waterheight)
+                waterheight_change_measured = ints.waterheight_total[end] -
+                                              ints.waterheight_total[1]
+                @test isapprox($change_waterheight, waterheight_change_measured,
+                               atol = $atol, rtol = $rtol)
+            end
+
+            if !isnothing($change_velocity)
+                velocity_change_measured = ints.velocity[end] - ints.velocity[1]
+                @test isapprox($change_velocity, velocity_change_measured, atol = $atol,
+                               rtol = $rtol)
+            end
+
+            if !isnothing($change_entropy)
+                entropy_change_measured = ints.entropy[end] - ints.entropy[1]
+                @test isapprox($change_entropy, entropy_change_measured, atol = $atol,
+                               rtol = $rtol)
+            end
+
+            if !isnothing($lake_at_rest)
+                lake_at_rest_measured = ints.lake_at_rest_error[end]
+                @test isapprox($lake_at_rest, lake_at_rest_measured, atol = $atol,
+                               rtol = $rtol)
+            end
+        end
+        println("═"^100)
+        println("\n\n")
+    end
+end
+
+# Get the first value assigned to `keyword` in `args` and return `default_value`
+# if there are no assignments to `keyword` in `args`.
+function get_kwarg(args, keyword, default_value)
+    val = default_value
+    for arg in args
+        if arg.head == :(=) && arg.args[1] == keyword
+            val = arg.args[2]
+            break
+        end
+    end
+    return val
+end
+
 """
     @trixi_testset "name of the testset" #= code to test #=
 
@@ -16,6 +142,7 @@ macro trixi_testset(name, expr)
     # loading structured, curvilinear meshes. Thus, we need to use a plain
     # module name here.
     quote
+        local time_start = time_ns()
         @eval module TrixiTestModule
         using Test
         using DispersiveShallowWater
@@ -30,6 +157,10 @@ macro trixi_testset(name, expr)
         end
         @testset $name $expr
         end
+        local time_stop = time_ns()
+        flush(stdout)
+        @info("Testset "*$name*" finished in "
+              *string(1.0e-9 * (time_stop - time_start))*" seconds.\n")
         nothing
     end
 end
