9baa86f2932f671682e609f4b6a4401a904b6e0d
Joshua Lampert <joshua.lampert@tuhh.de>
2023-09-26 12:38:11 +0200

    adjust test values

--- test/test_svaerd_kalisch_1d.jl
+++ test/test_svaerd_kalisch_1d.jl
@@ -13,26 +13,26 @@ EXAMPLES_DIR = joinpath(examples_dir(), "svaerd_kalisch_1d")
                                      "svaerd_kalisch_1d_dingemans.jl"),
                             tspan=(0.0, 1.0),
                             N=512,
-                            l2=[0.2264997669219955 0.7326153404586632 0.0],
-                            linf=[0.03641032763299723 0.11738950764868172 0.0],
-                            cons_error=[3.979039320256561e-13 7.648176553013466e-5 0.0],
+                            l2=[0.22648616606810756 0.7325813437573053 0.0],
+                            linf=[0.03645879748792702 0.11849678361651249 0.0],
+                            cons_error=[3.979039320256561e-13 7.670877062280329e-5 0.0],
                             change_waterheight=-3.979039320256561e-13,
-                            change_momentum=-2.0323272786892943e-9,
-                            change_entropy=-0.0005894453857990811,
-                            change_entropy_modified=3.0048763619561214e-6)
+                            change_momentum=-2.109259079896564e-9,
+                            change_entropy=-0.0005910026394531087,
+                            change_entropy_modified=3.20516357987799e-6)
     end
 
     @trixi_testset "svaerd_kalisch_1d_dingemans_upwind" begin
         @test_trixi_include(joinpath(EXAMPLES_DIR,
                                      "svaerd_kalisch_1d_dingemans_upwind.jl"),
                             tspan=(0.0, 1.0),
-                            l2=[0.2265797299117127 0.7327963925248041 0.0],
-                            linf=[0.036426119662156475 0.1178108264598402 0.0],
-                            cons_error=[1.1368683772161603e-13 7.540163948846978e-5 0.0],
+                            l2=[0.22656501380746077 0.7327645752975466 0.0],
+                            linf=[0.03648151661791921 0.1182504804050214 0.0],
+                            cons_error=[1.1368683772161603e-13 7.59953335162671e-5 0.0],
                             change_waterheight=-1.1368683772161603e-13,
-                            change_momentum=-2.1784861692353275e-9,
-                            change_entropy=-0.0005782276325589919,
-                            change_entropy_modified=1.7219122128153685e-6)
+                            change_momentum=-2.28489619585881e-9,
+                            change_entropy=-0.0005840425510541536,
+                            change_entropy_modified=2.108449052684591e-6)
     end
 
     @trixi_testset "svaerd_kalisch_1d_dingemans_relaxation" begin
@@ -40,11 +40,11 @@ EXAMPLES_DIR = joinpath(examples_dir(), "svaerd_kalisch_1d")
                                      "svaerd_kalisch_1d_dingemans_relaxation.jl"),
                             tspan=(0.0, 1.0),
                             N=512,
-                            l2=[0.22649926749071012 0.7326132878628044 0.0],
-                            linf=[0.03641021393801758 0.11738893742931189 0.0],
-                            cons_error=[3.979039320256561e-13 7.647827842231048e-5 0.0],
+                            l2=[0.22648560566941997 0.7325790781930053 0.0],
+                            linf=[0.03645867682442583 0.1184963632507331 0.0],
+                            cons_error=[3.979039320256561e-13 7.670526860626775e-5 0.0],
                             change_waterheight=-3.979039320256561e-13,
-                            change_entropy=-0.0005922598786582967,
+                            change_entropy=-0.0005940058100577517,
                             change_entropy_modified=0.0)
     end
 
