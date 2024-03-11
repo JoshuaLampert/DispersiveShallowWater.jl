2c9e146251c9395e93c53824b52145e86388c681
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-03-11 18:34:28 +0100

    add test using ExplicitImports.jl

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,12 +1,15 @@
 module TestAqua
 
 using Aqua
+using ExplicitImports
 using Test
 using DispersiveShallowWater
 
 @testset "Aqua.jl" begin
     Aqua.test_all(DispersiveShallowWater,
                   ambiguities = false)
+    @test isnothing(check_no_implicit_imports(DispersiveShallowWater))
+    @test isnothing(check_no_stale_explicit_imports(DispersiveShallowWater))
 end
 
 end #module 
