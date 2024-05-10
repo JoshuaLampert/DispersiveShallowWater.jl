5c2b3bcd614a65cb3cb183a98ca188bdd1aed1be
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-05-10 16:10:42 +0200

    fix reference

--- src/equations/bbm_bbm_1d.jl
+++ src/equations/bbm_bbm_1d.jl
@@ -108,7 +108,7 @@ end
     source_terms_manufactured_reflecting(q, x, t, equations::BBMBBMEquations1D, mesh)
 
 A smooth manufactured solution for reflecting boundary conditions in combination
-with [`initial_condition_manufactured_reflective`](@ref).
+with [`initial_condition_manufactured_reflecting`](@ref).
 """
 function source_terms_manufactured_reflecting(q, x, t, equations::BBMBBMEquations1D)
     g = equations.gravity
