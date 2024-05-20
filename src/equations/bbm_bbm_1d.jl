87611aea72e80d5410e28945102a593780856edf
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2024-05-20 21:28:24 +0200

    Update src/equations/bbm_bbm_1d.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/equations/bbm_bbm_1d.jl
+++ src/equations/bbm_bbm_1d.jl
@@ -145,7 +145,7 @@ function create_cache(mesh, equations::BBMBBMEquations1D,
     M = mass_matrix(solver.D1)
     Pd = BandedMatrix((-1 => fill(one(real(mesh)), N - 2),), (N, N - 2))
     D2d = (sparse(solver.D2) * Pd)[2:(end - 1), :]
-    # homogeneous Dirichtlet boundary conditions
+    # homogeneous Dirichlet boundary conditions
     invImD2d = inv(I - 1 / 6 * D^2 * D2d)
     m = diag(M)
     m[1] = 0
