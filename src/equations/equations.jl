519902c48e9d561ea53cb07da0be780de948ffec
Hendrik Ranocha <mail@ranocha.de>
2024-08-13 19:20:03 +0200

    document bathymetry

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -173,6 +173,17 @@ end
 end
 
 """
+    bathymetry(q, equations)
+
+Return the bathymetry of the primitive variables `q` for a given set of
+`equations`.
+
+`q` is a vector of the primitive variables at a single node, i.e., a vector
+of the correct length `nvariables(equations)`.
+"""
+function bathymetry end
+
+"""
     entropy(q, equations)
 
 Return the entropy of the primitive variables `q` for a given set of
