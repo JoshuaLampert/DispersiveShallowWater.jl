33115e34646ba68dc8e6a4537337f07ee8f2c437
Hendrik Ranocha <mail@ranocha.de>
2024-08-17 06:34:03 +0200

    explain mild slope

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -358,7 +358,8 @@ struct BathymetryMildSlope <: AbstractBathymetry end
     bathymetry_mild_slope = DispersiveShallowWater.BathymetryMildSlope()
 
 A singleton struct indicating a variable bathymetry with mild-slope
-approximation.
+approximation. Typically, this means that some terms like ``b_x^2``
+are neglected.
 
 See also [`bathymetry_flat`](@ref) and [`bathymetry_variable`](@ref).
 """
