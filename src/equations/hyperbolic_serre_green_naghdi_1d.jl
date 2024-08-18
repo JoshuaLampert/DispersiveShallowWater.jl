88eb387f4b0471bfe86bdf7dc583998d3629eb83
Hendrik Ranocha <mail@ranocha.de>
2024-08-18 06:53:53 +0200

    more docs for initial_condition_soliton

--- src/equations/hyperbolic_serre_green_naghdi_1d.jl
+++ src/equations/hyperbolic_serre_green_naghdi_1d.jl
@@ -88,7 +88,11 @@ end
     initial_condition_soliton(x, t, equations::HyperbolicSerreGreenNaghdiEquations1D, mesh)
 
 A soliton solution of the [`SerreGreenNaghdiEquations1D`](@ref)
-used for convergence tests in a periodic domain.
+used for convergence tests in a periodic domain. This is physically the
+same as [`initial_condition_convergence_test`](@ref) for the
+[`SerreGreenNaghdiEquations1D`](@ref). Please note that this is not an
+exact solution of the [`HyperbolicSerreGreenNaghdiEquations1D`](@ref)
+(only in the limit of the relaxation parameter ``\\lambda \\to \\infty``).
 
 See also [`initial_condition_convergence_test`](@ref).
 """
