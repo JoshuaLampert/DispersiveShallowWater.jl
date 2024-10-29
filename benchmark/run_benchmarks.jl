61981f06d0c367085952fad5ef2b48105559602a
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-10-29 11:27:56 +0100

    move develop after activate

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -1,6 +1,6 @@
 using Pkg
-Pkg.develop(PackageSpec(path = dirname(@__DIR__)))
 Pkg.activate(@__DIR__)
+Pkg.develop(PackageSpec(path = dirname(@__DIR__)))
 Pkg.instantiate()
 
 include("benchmarks.jl")
