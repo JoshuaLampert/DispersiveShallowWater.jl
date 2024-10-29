31f195691189cab5b143745ec82c8c8f1fc7e648
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-10-29 12:06:54 +0100

    use @__MODULE__ in trixi_include

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -19,9 +19,8 @@ elixirs = [joinpath(examples_dir(), "bbm_1d", "bbm_1d_basic.jl"),
 for elixir in elixirs
     benchname = joinpath(basename(dirname(elixir)), basename(elixir))
     println("Running $benchname...")
-    # use include instead of trixi_include(elixir, tspan = (0.0, 1e-10)) because AirspeedVelocity.jl has namespace issues
     redirect_stdout(devnull) do
-        include(elixir)
+        trixi_include(@__MODULE__, elixir, tspan = (0.0, 1e-10))
     end
     SUITE[benchname] = @benchmarkable DispersiveShallowWater.rhs!($(similar(sol.u[end])),
                                                                   $(copy(sol.u[end])),
