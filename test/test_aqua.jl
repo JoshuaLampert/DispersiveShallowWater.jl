d88619e17da6effb9f25e90fa45996237ae4afe3
Hendrik Ranocha <mail@ranocha.de>
2025-05-08 09:18:59 +0200

    add tests using JET.jl

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,8 +1,32 @@
 @testitem "Aqua.jl" setup=[Setup] begin
     using Aqua
     using ExplicitImports: check_no_implicit_imports, check_no_stale_explicit_imports
+    using JET
+
+    # Aqua.jl
     Aqua.test_all(DispersiveShallowWater,
                   ambiguities = false)
+
+    # ExplicitImports.jl
     @test isnothing(check_no_implicit_imports(DispersiveShallowWater))
     @test isnothing(check_no_stale_explicit_imports(DispersiveShallowWater))
+
+    # JET.jl
+    # With the default settings as of 2025-05-08, JET.jl
+    # reports issues originating from RecipesBase.jl.
+    # The only way to ignore them seems to be something like the
+    # following hack inspired by the disucssion in
+    # https://github.com/aviatesk/JET.jl/issues/570
+    struct IgnoreReciesBase end
+    function JET.match_module(::IgnoreReciesBase,
+                              @nospecialize(report::JET.InferenceErrorReport))
+        s = "MethodInstance for RecipesBase.apply_recipe"
+        any(report.vst) do vst
+            occursin(s, string(vst))
+        end
+    end
+    test_package(DispersiveShallowWater;
+                 target_defined_modules = true,
+                 ignored_modules = (IgnoreReciesBase(),))
+
 end
