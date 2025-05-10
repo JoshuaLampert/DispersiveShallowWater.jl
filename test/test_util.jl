8f6e37fade15a835f14b4ff503ddedf16f3771f6
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-05-10 09:28:06 +0200

    Use TrixiTest.jl (#195)
    * use TrixiTest.jl
    
    * forgot quote

--- test/test_util.jl
+++ test/test_util.jl
@@ -1,4 +1,5 @@
 using Test: @test
+using TrixiTest: @trixi_test_nowarn
 
 # Use a macro to avoid world age issues when defining new initial conditions etc.
 # inside an example.
@@ -175,45 +176,12 @@ macro test_allocations(semi, sol, allocs)
     end
 end
 
-# Copied from TrixiBase.jl. See https://github.com/trixi-framework/TrixiBase.jl/issues/9.
-"""
-    @test_nowarn_mod expr
-Modified version of `@test_nowarn expr` that prints the content of `stderr` when
-it is not empty and ignores some common info statements printed in DispersiveShallowWater.jl
-uses.
-"""
-macro test_nowarn_mod(expr, additional_ignore_content = String[])
+macro test_nowarn_mod(expr, additional_ignore_content = [])
     quote
-        let fname = tempname()
-            try
-                ret = open(fname, "w") do f
-                    redirect_stderr(f) do
-                        $(esc(expr))
-                    end
-                end
-                stderr_content = read(fname, String)
-                if !isempty(stderr_content)
-                    println("Content of `stderr`:\n", stderr_content)
-                end
-
-                # Patterns matching the following ones will be ignored. Additional patterns
-                # passed as arguments can also be regular expressions, so we just use the
-                # type `Any` for `ignore_content`.
-                ignore_content = Any["[ Info: You just called `trixi_include`. Julia may now compile the code, please be patient.\n",
-                                     r"┌ Warning: The still-water surface needs to be 0 for the BBM-BBM equations\n└ @ DispersiveShallowWater .*\n"]
-                append!(ignore_content, $additional_ignore_content)
-                for pattern in ignore_content
-                    stderr_content = replace(stderr_content, pattern => "")
-                end
-
-                # We also ignore simple module redefinitions for convenience. Thus, we
-                # check whether every line of `stderr_content` is of the form of a
-                # module replacement warning.
-                @test occursin(r"^(WARNING: replacing module .+\.\n)*$", stderr_content)
-                ret
-            finally
-                rm(fname, force = true)
-            end
-        end
+        add_to_additional_ignore_content = [
+            r"┌ Warning: The still-water surface needs to be 0 for the BBM-BBM equations\n└ @ DispersiveShallowWater .*\n"
+        ]
+        append!($additional_ignore_content, add_to_additional_ignore_content)
+        @trixi_test_nowarn $(esc(expr)) $additional_ignore_content
     end
 end
